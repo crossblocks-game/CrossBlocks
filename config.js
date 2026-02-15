7491a03742b25f50e2cf6ca7c2ec6f21a5ec75f8
crossblocks-game <crossblocks.game@gmail.com>
2026-02-15 18:22:10 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+// CONFIGURATION ADMIN
+
+const CONFIG = {
+
+themes: [
+    "metal",
+    "dark",
+    "military",
+    "parchment"
+],
+
+weapons: {
+    "Blaster": {degats:10, portee:30},
+    "Fusil lourd": {degats:18, portee:40},
+    "Lance-roquettes": {degats:30, portee:60},
+    "Sabre laser": {degats:25, portee:1}
+},
+
+siteName: "Générateur de Cartes"
+
+};
